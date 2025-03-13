c2bc8f57eca64f42188e4a88e81941532cb3eb58
Lewis Reed <Lewisreed.352@gmail.com>
2025-03-13 19:03:46 +0000

    built a simple sat solver. checks literals in a clause and if it finds a true value, breaks. if a clause is found to be false, breaks out the assignment. need to convert assignment to proper output form

--- zpkd88.py
+++ zpkd88.py
@@ -20,7 +20,40 @@ def load_dimacs(file_name):
 
 
 def simple_sat_solve(clause_set):
-    ...
+    variables = set()
+    for clause in clause_set:
+        for literal in clause:
+            variables.add(abs(literal))
+    assignments = []
+    for i in range(2 ** len(variables)):
+        binary = bin(i)[2:]
+        binary = '0' * (len(variables) - len(binary)) + binary
+        current_assignment = []
+        for bit in binary:
+            if bit == "0":
+                current_assignment.append(False)
+            else:
+                current_assignment.append(True)
+        assignments.append(current_assignment)
+    successful_assignment = []
+    for assignment in assignments:
+        satisfies = True
+        for clause in clause_set:
+            clause_evaluation = False
+            for literal in clause:
+                if literal > 0:
+                    if assignment[literal - 1]:
+                        clause_evaluation = True
+                        break
+                else:
+                    if not assignment[abs(literal) - 1]:
+                        clause_evaluation = True
+                        break
+            if clause_evaluation == False:
+                satisfies = False
+                break
+        if satisfies:
+            successful_assignment.append(assignment)
 
 
 def branching_sat_solve(clause_set,partial_assignment):
@@ -45,14 +78,14 @@ def test():
     except:
         print("Failed to correctly load DIMACS file")
 
-#     print("Testing simple_sat_solve")
-#     try:
-#         sat1 = [[1],[1,-1],[-1,-2]]
-#         check = simple_sat_solve(sat1)
-#         assert check == [1,-2] or check == [-2,1]
-#         print("Test (SAT) passed")
-#     except:
-#         print("simple_sat_solve did not work correctly a sat instance")
+    print("Testing simple_sat_solve")
+    try:
+        sat1 = [[1],[1,-1],[-1,-2]]
+        check = simple_sat_solve(sat1)
+        assert check == [1,-2] or check == [-2,1]
+        print("Test (SAT) passed")
+    except:
+        print("simple_sat_solve did not work correctly a sat instance")
 
 #     try:
 #         unsat1 = [[1, -2], [-1, 2], [-1, -2], [1, 2]]
